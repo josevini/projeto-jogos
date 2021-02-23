90124cfd9a7494c95b6698de519fa211200a1d3b
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-22 21:37:50 -0300

    Create modelo.php

---
+++ jogos/modelo.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+    <head lang="pt-br">
+        <meta chaset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="estilos/estilo.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <?php
+            require_once "includes/banco.php";
+            require_once "includes/login.php";
+            require_once "includes/funcoes.php";
+        ?>
+    </head>
+
+    <body>
+        <div id="corpo">
+            <?php
+                
+            ?>
+        </div>
+        <?php require_once "rodape.php"?>
+    </body>
+</html>
